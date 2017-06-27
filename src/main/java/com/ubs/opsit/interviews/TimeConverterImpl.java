d04ce87a1b00f9aa05c47b68820dec3cc06ebd2f
Kunwar <kunwar.nitw@gmail.com>
2017-06-27 21:31:13 +0530

    Adding the implementation class for Interface TimeConverter.java

---
+++ src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.ubs.opsit.interviews;
+
+/**
+ * @author Kunwar
+ *
+ */
+public class TimeConverterImpl implements TimeConverter {
+
+	/* (non-Javadoc)
+	 * @see com.ubs.opsit.interviews.TimeConverter#convertTime(java.lang.String)
+	 */
+	@Override
+	public String convertTime(String aTime) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
