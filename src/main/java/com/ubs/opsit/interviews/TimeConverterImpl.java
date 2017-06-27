05b5ce5397d10f72d5c2b3fa57a18959fa79ce74
Kunwar <kunwar.nitw@gmail.com>
2017-06-27 22:08:01 +0530

    Implementation of Berlin Clock in Java 8.

--- src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
+++ src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
@@ -3,27 +3,101 @@
  */
 package com.ubs.opsit.interviews;
 
+import java.util.stream.Stream;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.Validate;
+
 /**
  * @author Kunwar
  *
  */
 public class TimeConverterImpl implements TimeConverter {
 
-	/* (non-Javadoc)
-	 * @see com.ubs.opsit.interviews.TimeConverter#convertTime(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.ubs.opsit.interviews.TimeConverter#convertTime(java.lang.String)\
+	 * This method will take time as String and convert it to Berlin Time
 	 */
 	@Override
 	public String convertTime(String aTime) {
-		// TODO Auto-generated method stub
-		return null;
+
+		checkForNull(aTime);
+		checkForCorrectPattern("\\d\\d:\\d\\d:\\d\\d", aTime, "aTime");
+
+		int[] parts = Stream.of(aTime.split(":")).mapToInt(Integer::parseInt).toArray();
+		StringBuffer res = new StringBuffer();
+		res.append(getSec(parts[2])).append("\n");
+		res.append(getHoursOne(parts[0])).append("\n");
+		res.append(getHoursTwo(parts[0])).append("\n");
+		res.append(getMinutesOne(parts[1])).append("\n");
+		res.append(getMinutesTwo(parts[1])).append("\n");
+		return res.toString();
 	}
 
 	/**
-	 * @param args
+	 * Validates the null input
+	 * 
+	 * @param inputTime
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private void checkForNull(String inputTime) {
+		Validate.notNull(inputTime, "The object aTime must not be null");
+	}
 
+	/**
+	 * Validates the input pattern
+	 * 
+	 * @param pattern
+	 * @param input
+	 * @param inputName
+	 */
+	private void checkForCorrectPattern(String pattern, String input, String inputName) {
+		Validate.isTrue(input.matches(pattern),
+				"Argument `" + inputName + "` argument should match " + pattern + " pattern");
+	}
+
+	protected String getSec(int number) {
+		if (number % 2 == 0)
+			return "Y";
+		else
+			return "O";
+	}
+
+	protected String getHoursOne(int number) {
+		return getOnOff(4, getTopNumberOfOnSigns(number));
+	}
+
+	protected String getHoursTwo(int number) {
+		return getOnOff(4, number % 5);
+	}
+
+	protected String getMinutesOne(int number) {
+		return getOnOff(11, getTopNumberOfOnSigns(number), "Y").replaceAll("YYY", "YYR");
+	}
+
+	protected String getMinutesTwo(int number) {
+		return getOnOff(4, number % 5, "Y");
+	}
+
+	private String getOnOff(int lamps, int onSigns) {
+		return getOnOff(lamps, onSigns, "R");
+	}
+
+	private String getOnOff(int lamps, int onSigns, String onSign) {
+		String out = "";
+		for (int i = 0; i < onSigns; i++) {
+			out += onSign;
+		}
+		for (int i = 0; i < (lamps - onSigns); i++) {
+			out += "O";
+		}
+		return out;
+	}
+
+	private int getTopNumberOfOnSigns(int number) {
+		return (number - (number % 5)) / 5;
 	}
 
 }
